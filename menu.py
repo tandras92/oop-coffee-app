31f96496722a3b9efece912dcc457744d4b7b434
Taylor <tandras92@outlook.com>
2021-07-26 20:54:10 -0500

    updated view option in menu

--- menu.py
+++ menu.py
@@ -29,6 +29,7 @@ class Menu:
             MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5),
             MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3),
         ]
+
         self.options = {
             "1": "View Menu",
             "2": "Search Drinks",
@@ -47,8 +48,9 @@ class Menu:
         options = ""
         option_number = 1
         for item in self.menu:
-            options += f"{str(option_number)}. {item.name}\n"
+            options += f"{option_number} {item.name} "
             option_number += 1
+        options = options.split()
         return options
 
     def find_drink(self, order_name):
@@ -59,6 +61,7 @@ class Menu:
         print("Sorry that item is not available.")
 
     def menu_selection(self):
+        """Asks user to select an option from the menu"""
         print("Please select from the following options:")
         menu_options = self.options
         menu_items = self.get_items()
@@ -70,11 +73,18 @@ class Menu:
         for option in menu_options:
             if option == "1" and option == user_selection:
                 print("Please Select a Beverage:")
-                print(menu_items)
+                # convert list to dict
+                option_dict = dict(zip(menu_items[::2], menu_items[1::2]))
+                for k, v in option_dict.items():
+                    print(f"{k}. {v}")
                 beverage_choice = input("\nSelection: ")
-                for drink_option in menu_items:
+                for drink_option in option_dict:
                     if drink_option == "1" and drink_option == beverage_choice:
-                        ...
+                        return option_dict.get('1')
+                    elif drink_option == "2" and drink_option == beverage_choice:
+                        return option_dict.get('2')
+                    elif drink_option == "3" and drink_option == beverage_choice:
+                        return option_dict.get('3')
             elif option == "2" and option == user_selection:
                 order_name = input("Please enter the name of drink: ")
                 self.find_drink(order_name)
